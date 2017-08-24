233636cb96d5af6947b8e9f57a262b54b0763985
Mr-Nancy <mister.nansi@gmail.com>
2017-08-24 17:56:08 +0300

    Temp

--- extensions.js
+++ extensions.js
@@ -15,11 +15,24 @@
         return parseInt(number,10).toString(2);
     };
 
+    ext.getTemperature = function(city, callback) {
+        var xmlhttp = new XMLHttpRequest();
+        xmlhttp.open("GET", "http://weathers.co/api.php?city="+city, true);
+        xmlhttp.onreadystatechange = function() {
+            if (this.readyState == 4 && this.status == 200) {
+                var myObj = JSON.parse(this.responseText);
+                callback(myObj.data.temperature);
+            }
+        };
+        xmlhttp.send();
+    };
+
     // Block and block menu descriptions
     var descriptor = {
         blocks: [
             ['', 'Dummy block', 'dummy'],
             ['r', 'binary %n', 'toBinary', 1],
+            ['R', 'current temperature in city %s', 'getTemperature', 'Boston'],
         ]
     };
 
