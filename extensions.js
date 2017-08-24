fc042dcceea19d92d7a617788dfbf35015f8aed1
Mr-Nancy <mister.nansi@gmail.com>
2017-08-24 16:20:02 +0300

    Binary block

--- extensions.js
+++ extensions.js
@@ -12,13 +12,10 @@
     };
 
     ext.toBinary = function(number) {
-        var result = "";
-
-        while (number > 2) {
-            result = number % 2 + result;
-            number = number / 2;
-        }
-        return result;
+        var out = "";
+        while(length--)
+          out += (dec >> length ) & 1;    
+        return out; 
     };
 
     // Block and block menu descriptions
